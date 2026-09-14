4f30822c3bf560308627ea3d5fe922d269d20cd4
Milutin Nikodijevic <milutin.nikodijevic@gmail.com>
2026-09-14 03:10:07 +0200

    Dodat kontroler i ukljucena provera dubine

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -2,6 +2,13 @@
 #include <memory>
 #include <spdlog/spdlog.h>
 
+class MainController : public engine::core::Controller {
+protected:
+    void initialize() override {
+        engine::graphics::OpenGL::enable_depth_testing();
+    }
+};
+
 class MyApp : public engine::core::App {
 
 public:
@@ -9,6 +16,9 @@ public:
 };
 
 void MyApp::app_setup() {
+    auto main_controller = register_controller<MainController>();
+    main_controller->after(
+            engine::core::Controller::get<engine::core::EngineControllersEnd>());
     spdlog::info("Hello, setup!");
 }
 
