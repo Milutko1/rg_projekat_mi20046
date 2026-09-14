3c9b2eec33ea8142d72b5cc412d9fde7fb268eff
Milutin Nikodijevic <milutin.nikodijevic@gmail.com>
2026-09-14 22:22:17 +0200

    Dodato ambijentalno i difuzno osvetljenje

--- app/resources/shaders/basketball.glsl
+++ app/resources/shaders/basketball.glsl
@@ -8,9 +8,14 @@ out vec2 TexCoords;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+
+layout (location = 1) in vec3 aNormal;
+out vec3 Normal;
+
 void main() {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
     TexCoords = aTexCoords;
+    Normal = mat3(transpose(inverse(model))) * aNormal;
 }
 
 //#shader fragment
@@ -18,8 +23,19 @@ void main() {
 out vec4 FragColor;
 
 in vec2 TexCoords;
+in vec3 Normal;
+
 uniform sampler2D texture_diffuse1;
 
 void main() {
-    FragColor = vec4(texture(texture_diffuse1, TexCoords).rgb, 1.0);
+    vec3 norm = normalize(Normal);
+    vec3 light_direction = normalize(vec3(1.0, 1.0, 1.0));
+
+    float diffuse = max(dot(norm, light_direction), 0.0);
+    float ambient = 0.2;
+
+    vec3 texture_color = texture(texture_diffuse1, TexCoords).rgb;
+    vec3 result = (ambient + 0.8 * diffuse) * texture_color;
+
+    FragColor = vec4(result, 1.0);
 }
